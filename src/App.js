512cf280acc39ab0968e9b2156b4853812b1467d
victorydf0901 <dave.superf0901@gmail.com>
2022-04-11 22:46:44 +0300

    working on App.js, List.jsx, Map.jsx, PlaceDetails.jsx

--- src/App.js
+++ src/App.js
@@ -8,10 +8,13 @@ import Map from "./Components/Map/Map";
 
 const App = () => {
     const [places, setPlaces] = useState([])
+    const [childClicked, setChildClicked] = useState(null)
 
     const [coordinates, setCoordinates] = useState({})
 
     const [bounds, setBounds] = useState({})
+
+    const [isLoading, setIsLoading] = useState(false)
 
     useEffect(() => {
         navigator.geolocation.getCurrentPosition(({ coords: {latitude, longitude} }) => {
@@ -20,9 +23,12 @@ const App = () => {
     }, [])
 
     useEffect(() => {
+        setIsLoading(true)
+
         getPlacesData(bounds.sw, bounds.ne)
             .then((data) => {
                 setPlaces(data.data);
+                setIsLoading(false)
             })
     }, [coordinates, bounds])
 
@@ -34,6 +40,8 @@ const App = () => {
                 <Grid item xs={12} md={4}>
                     <List
                         places={places}
+                        childClicked={childClicked}
+                        isLoading={isLoading}
                     />
                 </Grid>
                 <Grid item xs={12} md={8}>
@@ -42,6 +50,7 @@ const App = () => {
                         setCoordinates={setCoordinates}
                         setBounds={setBounds}
                         places={places}
+                        setChildClicked={setChildClicked}
                     />
                 </Grid>
             </Grid>
--- src/Components/Map/Map.jsx
+++ src/Components/Map/Map.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useState} from 'react'
 import GoogleMapReact from 'google-map-react'
 import { Paper, Typography, useMediaQuery } from '@material-ui/core'
 import LocationOnOutlined from '@material-ui/icons/LocationOnOutlined'
@@ -6,10 +6,10 @@ import Rating from '@material-ui/lab/Rating'
 
 import useStyles from './styles'
 
-const Map = ({coordinates, setCoordinates, setBounds, places}) => {
+const Map = ({coordinates, setCoordinates, setBounds, places, setChildClicked}) => {
     const classes = useStyles()
     const isDesktop = useMediaQuery('(min-width:600px)')
-    
+
     return(
         <div className={classes.mapContainer}> 
             <GoogleMapReact
@@ -29,7 +29,7 @@ const Map = ({coordinates, setCoordinates, setBounds, places}) => {
                         sw: e.marginBounds.sw
                     })
                 }}
-                onChildClick={(child) => {}}
+                onChildClick={(child) => setChildClicked(child)}
             >
                 {
                     places?.map((place, i) => (
